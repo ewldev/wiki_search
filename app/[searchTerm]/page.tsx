5297a5a8c36efd22a82fe2872fa0584a33532f4f
ewldev <edwin.leung2@gmail.com>
2023-03-30 17:17:37 -0400

    added dynamic route and get getWikiResults function

---
+++ app/[searchTerm]/page.tsx
@@ -0,0 +1,11 @@
+type Props = {
+    params: {
+        searchTerm: string
+    }
+}
+
+export default function pgenerateMetadata({ params: { searchTerm } }: Props) {
+  return (
+    <div>page</div>
+  )
+}
